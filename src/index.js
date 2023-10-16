ed69937d6bdb1d6689fc406362b9e0c7d3d72f85
hamza1120 <hamzabouhouchi02@gmail.com>
2023-10-16 14:24:48 +0100

    v2

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux"
-import {BrowserRouter as Router} from "react-router-dom"
-import store from './redux/store';
-import axios from "axios"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import store from "./redux/store";
+import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api"
+axios.defaults.baseURL = "https://mern-todo-api-nuxt.onrender.com/api";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
   </Router>
 );
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
